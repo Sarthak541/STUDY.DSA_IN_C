568a6a5200e52e581cd982c423362620e2d8c61c
Sarthak <sr.sarthak.talukdar@gmail.com>
2026-04-02 15:54:16 -0400

    Added more boiler plate on the first file

--- part1_arrays/array.c
+++ part1_arrays/array.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+int gcd(int p, int q){
+    if(q==0) return p;
+    return gcd(q,p%q);
+}
+
 int main(){
+
+    int my_gcd = gcd(30,20);
+    printf("This is the gcd of 30 and 20 - %d\n",my_gcd);
+
 
     return 0;
 }
