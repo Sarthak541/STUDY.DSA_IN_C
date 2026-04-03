94cd6ac5ad898bb04d53a5e1c92a5d669678968c
Sarthak <sr.sarthak.talukdar@gmail.com>
2026-04-03 17:26:13 -0400

    Added selection sort logic

--- part2_sorting_algortihms/sorting.c
+++ part2_sorting_algortihms/sorting.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char* selection_sort(char* input){
+    int word_len = strlen(input);
+    for (int i = 0; i < word_len; ++i) {
+        input[i] = tolower(input[i]);
+    }
+    for (int i = 0; i < word_len-1; ++i) {
+        int smallest = i;
+        for (int j = i+1; j < word_len; ++j) {
+            if (input[j] < input[smallest]) {
+                smallest = j;
+            }
+        }
+        char temp = input[i];
+        input[i] = input[smallest];
+        input[smallest] = temp;
+    }
+    return input;
+}
 
 int main(){
-    
+    char selection_sort_input[] = "SELECTIONSORT";
+    printf("Now we are doing selection_sort \n"
+           "The value of selection sort is %s\n",
+           selection_sort(selection_sort_input));
     return 0;
 }
